eff955c7a0c8aace800c0431801b161d1b7dfed9
BindiPatel14 <harakishorec@hotmail.com>
2025-08-27 16:03:22 -0700

    Adding Emp Badge sys

--- src/Karat/EntryWithoutExits.java
+++ src/Karat/EntryWithoutExits.java
@@ -13,29 +13,49 @@ public class EntryWithoutExits {
 * */
     public static void main(String[] args) {
         String[][] r1 = {
-                {"P", "entry"},
-                {"P", "exit"}
+                {"Paul", "enter"},
+                {"Pauline", "exit"},
+                {"Paul", "enter"},
+                {"Paul", "exit"},
+                {"Martha", "exit"},
+                {"Joe", "enter"},
+                {"Martha", "enter"},
+                {"Steve", "enter"},
+                {"Martha", "exit"},
+                {"Jennifer", "enter"},
+                {"Joe", "enter"},
+                {"Curtis", "exit"},
+                {"Curtis", "enter"},
+                {"Joe", "exit"},
+                {"Martha", "enter"},
+                {"Martha", "exit"},
+                {"Jennifer", "exit"},
+                {"Joe", "enter"},
+                {"Joe", "enter"},
+                {"Martha", "exit"},
+                {"Joe", "exit"},
+                {"Joe", "exit"}
         };
-
 
         String[][] r2 = {
-                {"P", "entry"},
-                {"P", "entry"}
+                {"Paul", "enter"},
+                {"Paul", "exit"}
         };
 
-
         String[][] r3 = {
-                {"P", "exit"},
-                {"P", "entry"}
+                {"Paul", "enter"},
+                {"Paul", "enter"},
+                {"Paul", "exit"},
+                {"Paul", "exit"}
         };
 
         String[][] r4 = {
-                {"R", "entry"},
-                {"P", "entry"},
-                {"P", "exit"},
-                {"P", "exit"},
-                {"P", "entry"},
-                {"R", "entry"}
+                {"Raj", "enter"},
+                {"Paul", "enter"},
+                {"Paul", "exit"},
+                {"Paul", "exit"},
+                {"Paul", "enter"},
+                {"Raj", "enter"}
         };
 
         System.out.println(getEntryExitLists(r1));
@@ -45,51 +65,54 @@ public class EntryWithoutExits {
 
     }
 
-/*
-* Input is a 2-D array of records and we will return List <List<String>>
-*
-* */
-    public static List<List<String>> getEntryExitLists(String[][] records){
-        List<String> entryWithoutExitsList = new ArrayList<String>(); // Will have List of all employees with entry records without exits captured
-        List<String> exitsWithoutEntryList = new ArrayList<String>();// Will have List of all employees with exit records without entry captured
+    public static Map<String,Set<String>> getEntryExitLists(String[][] records){
+        Map<String, Set<String>> resultMap = new HashMap<>();
+        Set<String> entryWithoutExit = new HashSet<>();
+        Set<String> exitWithoutEntry = new HashSet<>();
 
-        List<List<String>> result = new ArrayList<List<String>>();
+        // Track if an employee is currently "inside" (true) or "outside" (false)
+        Map<String, Boolean> inside = new HashMap<>();
 
-        result.add(entryWithoutExitsList);
-        result.add(exitsWithoutEntryList);
-        Set<String> entrySet = new HashSet<String>(); // Set to maintain all unique employees who wil be part of entryWithoutExitsList
-        Set<String> exitSet = new HashSet<String>(); // Set to maintain all unique employees who wil be part of exitsWithoutEntryList
-        Map<String,Integer> entryMap = new HashMap<String,Integer>(); // Map to track entry for each employee
+        for (String[] record : records) {
+            String empName = record[0];
+            String empAction = record[1];
 
+            boolean isInside = inside.getOrDefault(empName, false);
 
-        for(String[] s : records) {
-            // We will have two possible values for events "Entry/Exit"
-            if(s[1].equalsIgnoreCase("entry")){
-                if(entryMap.containsKey(s[0])){
-                    entrySet.add(s[0]); // If entry was already captured previously then it will be present in entryMap for the employee --> i.e. employee exited without swiping his badge and reentered the room so needs to be part of entryWithoutExitList
-                }else{
-                    entryMap.put(s[0],0); // Capturing new entry for employee ( Can be either brand new or reentry after exit was captured)
+            if (empAction.equalsIgnoreCase("enter")) {
+                // Enter while already inside -> Entry without Exit
+                if (isInside) {
+                    entryWithoutExit.add(empName);
                 }
-            }else{
-                if(!entryMap.containsKey(s[0])){ // If event is Exit and no entry for employee is found in entry --> need to add to exit without entry List
-                    exitSet.add(s[0]);
-                }else{
-                    entryMap.remove(s[0]); //If entry record for employee is found, then we simply delete the entry record from the map
+                inside.put(empName, true);
+            } else if (empAction.equalsIgnoreCase("exit")) {
+                // Exit while already outside -> Exit without Entry
+                if (!isInside) {
+                    exitWithoutEntry.add(empName);
+                } else {
+                    inside.put(empName, false);
                 }
             }
         }
 
-        //Need to capture all pending employees who only had entry captured and no exit captured
-        //Reason we have this is because in question it is stated that at end of List all employees have exited the room
-        entrySet.addAll(entryMap.keySet());
+        // Anyone still inside at the end -> Entry without Exit
+        for (Map.Entry<String, Boolean> e : inside.entrySet()) {
+            if (e.getValue()) {
+                entryWithoutExit.add(e.getKey());
+            }
+        }
 
-        entryWithoutExitsList.addAll(entrySet);
-        exitsWithoutEntryList.addAll(exitSet);
+        resultMap.put("Entry Without Exit", entryWithoutExit);
+        resultMap.put("Exit Without Entry", exitWithoutEntry);
 
+        return resultMap;
 
-
-        return result;
     }
+
+/*
+* Input is a 2-D array of records and we will return List <List<String>>
+*
+* */
 
 
 
